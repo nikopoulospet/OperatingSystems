f2c21b1bd28a4298a2e9a83018b817ae21ec2bce
nikopoulospet <xxpeteynikxx@gmail.com>
2020-09-09 18:03:29 -0400

    doit part 1 done

--- doit.C
+++ doit.C
@@ -8,24 +8,55 @@ using namespace std;
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/time.h>
+#include <sys/resource.h>
 
 extern char **environ;		/* environment info */
 
-char* which(){
+char* which(char *cmd){
+    /* This function finds the path of a given command */
+    /* Return: char* */
     FILE* fp;
-    char *data = (char*)malloc(1024 * sizeof(char));
-    const char *testdata = "which wc > temp.txt";
-    system(testdata);
+    long LSize;
+    /* Assemble and execute system command, pipe into "temp.txt" */
+    std::string command = "which " + std::string(cmd) + " > temp.txt";
+    system(command.c_str());
     fp = fopen("temp.txt", "r");
+
+    /* find the size of whatever is in file and allocate a memory space */
+    fseek(fp,0L,SEEK_END);
+    LSize = ftell(fp) + 1;
+    rewind(fp);
+    char *data = (char*)malloc(LSize * sizeof(char));
+
+    /* save data from file into the LoChars "data" */
     if (fp == NULL) perror ("error opening file");
     else {
-        while (fgets (data, 1024, fp) != NULL);
+        while (fgets (data, LSize, fp) != NULL);
     }
     fclose (fp);
+
+    /* remove "\n" and return data */
     std::string str = std::string(data);
     str.erase(strlen(data) - 1, 1);
     data = strdup(str.c_str());
     return data;
+}
+
+void getUsageData(struct timeval &start, struct timeval &end){
+    int who = RUSAGE_CHILDREN;
+    struct rusage usage;
+    
+    getrusage(who, &usage);
+    gettimeofday(&end, NULL);
+
+    cout << "CPU TIME (USER): " << (double) usage.ru_utime.tv_usec / 1000 << "\n"; // CPU TIME
+    cout << "CPU TIME (SYSTEM): " << (double) usage.ru_stime.tv_usec / 1000 << "\n";
+    cout << "WALL_CLOCK ELAPSED: " << ((end.tv_sec * 1000000 + end.tv_usec) - (start.tv_sec * 1000000 + start.tv_usec)) << "\n";
+    cout << "PREEMPTED INVOLUNTARILY: " << usage.ru_nivcsw << "\n";
+    cout << "PROCESS STOPPED VOLUNTARILY: " << usage.ru_nvcsw << "\n";
+    cout << "MAJOR PAGE FAULTS: " << usage.ru_minflt << "\n";
+    cout << "MINOR PAGE FAULTS: " << usage.ru_majflt << "\n";
 }
 
 main(int argc, char **argv)
@@ -34,8 +65,12 @@ main(int argc, char **argv)
 {
     char *argvNew[argc];
     int pid, i;
-    char *path = which();
+    char *path = which(argv[1]);
 
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
+
+    /* iterate through list of args from terminal, replace command w/ path of command */
     for (i=0;i<argc;i++){
         if (i == 0){
             /*find file path for command */
@@ -44,9 +79,6 @@ main(int argc, char **argv)
             argvNew[i] = argv[i+1];
         }
     }
-
-    argvNew[i+1] = NULL;
-
 
     if ((pid = fork()) < 0) {
 	cerr << "Fork error\n";
@@ -61,6 +93,8 @@ main(int argc, char **argv)
     }
     else {
         /* parent */
-	wait(0);		/* wait for the child to finish */
+	wait(0); //wait for the child to finish
+    getUsageData(start, end);
+
     }
 }
